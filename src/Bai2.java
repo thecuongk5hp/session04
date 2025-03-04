db57d52e194c76559edd52eb93891d8517122f59
thecuongk5hp <147390984+thecuongk5hp@users.noreply.github.com>
2025-03-05 01:49:35 +0700

    Update Bai2.java

--- src/Bai2.java
+++ src/Bai2.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 public class Bai2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("nhập vào số nguyên n:");
+        System.out.println("Nhập 1 số nguyên:");
         int n = sc.nextInt();
-        if(n<2){
-            System.out.println("không phải số nguyên tố!");
-
-        } else {
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0) {
-                    System.out.println("số nguyên tố!");
-                    break;
-                }else {
-                    System.out.println("không phải là số nguyên tố!");
+        boolean isPrime = true;
+        if(n < 2){
+            System.out.println("Không phải số nguyên tố");
+        }else{
+            for(int i = 2; i <= Math.sqrt(n); i++){
+                if(n % i == 0){
+                    System.out.printf("%d không phải là số nguyên tố", n);
+                    isPrime = false;
                 }
-                break;
+            }
+            if(isPrime){
+                System.out.printf("%d là số nguyên tố", n);
             }
         }
     }
