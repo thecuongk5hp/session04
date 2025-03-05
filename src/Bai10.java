43064364c1c8d6c058a7e91b40044fa166162bbf
thecuongk5hp <147390984+thecuongk5hp@users.noreply.github.com>
2025-03-05 09:08:35 +0700

    Update Bai10.java

--- src/Bai10.java
+++ src/Bai10.java
@@ -1,67 +1,82 @@
 import java.util.Scanner;
 public class Bai10 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        int choice;
         do {
-            System.out.println("----------------MENU----------------");
+            System.out.println("-------------------- MENU --------------------");
             System.out.println("1. Kiểm tra số nguyên tố");
             System.out.println("2. Kiểm tra số hoàn hảo");
             System.out.println("3. Tìm và tính tổng tất cả các ước của một số");
             System.out.println("4. Thoát");
-            System.out.printf("Lựa chọn của bạn: ");
-            int choice = sc.nextInt();
+            System.out.print("Lựa chọn của bạn: ");
+            choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.println("Nhập vào 1 số");
-                    int n = sc.nextInt();
+                    System.out.print("Nhập số nguyên dương: ");
+                    int num1 = scanner.nextInt();
                     boolean isPrime = true;
-                    if(n < 2){
-                        System.out.println("Không phải số nguyên tố");
-                    }else{
-                        for(int i = 2; i <= Math.sqrt(n); i++){
-                            if(n % i == 0){
-                                System.out.printf("%d không phải là số nguyên tố\n", n);
+                    if (num1 < 2) {
+                        isPrime = false;
+                    } else {
+                        for (int i = 2; i <= Math.sqrt(num1); i++) {
+                            if (num1 % i == 0) {
                                 isPrime = false;
+                                break;
                             }
                         }
-                        if(isPrime){
-                            System.out.printf("%d là số nguyên tố\n", n);
-                        }
+                    }
+                    if (isPrime) {
+                        System.out.println(num1 + " là số nguyên tố.");
+                    } else {
+                        System.out.println(num1 + " không phải là số nguyên tố.");
                     }
                     break;
                 case 2:
-                    System.out.println("Nhập vào 1 số");
-                    int num = sc.nextInt();
-                    int sumDivisors = 0;
-                    for (int i = 1; i < num; i++) {
-                        if(num % i == 0){
-                            sumDivisors += i;
+                    System.out.print("Nhập số cần kiểm tra: ");
+                    int num2 = scanner.nextInt();
+                    int sum1 = 0;
+                    if (num2 < 1){
+                        System.out.println("Số không hợp lệ");
+                        break;
+                    } else {
+                        for (int i = 1; i < num2; i++) {
+                            if (num2 % i == 0) {
+                                sum1 += i;
+                            }
                         }
                     }
-                    if(sumDivisors == num){
-                        System.out.printf("Số %d là số hoàn hảo\n", num);
-                    }else{
-                        System.out.printf("Số %d không là số hoàn hảo\n", num);
+                    if (sum1 == num2) {
+                        System.out.println(num2 + " là số hoàn hảo.");
+                    } else {
+                        System.out.println(num2 + " không phải là số hoàn hảo.");
                     }
                     break;
                 case 3:
-                    System.out.println("Nhập vào 1 số");
-                    int number = sc.nextInt();
-                    int sum = 0;
-                    System.out.println("Các ước số của " + number + " là");
-                    for (int i = 1; i <= number; i++) {
-                        if(number % i == 0){
-                            System.out.println(i);
-                            sum += i;
+                    System.out.print("Nhập số nguyên dương: ");
+                    int num3 = scanner.nextInt();
+                    int sum2 = 0;
+                    if (num3 < 1){
+                        System.out.println("Số không hợp lệ");
+                        break;
+                    } else {
+                        System.out.print("Các ước của " + num3 + " là: ");
+                        for (int i = 1; i <= num3; i++) {
+                            if (num3 % i == 0) {
+                                System.out.print(i + " ");
+                                sum2 += i;
+                            }
                         }
+                        System.out.println("\nTổng tất cả các ước của " + num3 + " là: " + sum2);
                     }
-                    System.out.printf("Tổng các ước của số %d là: %d\n", number, sum);
                     break;
                 case 4:
-                    System.exit(0);
+                    System.out.println("Thoát chương trình");
+                    break;
                 default:
-                    System.out.println("Vui long nhập lài từ 1 đến 4");
+                    System.out.println("Lựa chọn không hợp lệ! Vui lòng nhập từ 1 đến 4.");
             }
-        }while (true);
+        } while (choice != 4);
+        scanner.close();
     }
 }
